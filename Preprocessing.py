4438fa4ef119f68f142700fb1ba113f5359c523c
victorbjo <victor.bjoerholm@gmail.com>
2022-10-28 11:20:10 +0200

    Fixed cropping and scaling functions

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,31 +1,55 @@
 import cv2
 import numpy as np
  
-def preprocessing(img): 
+
+def scale(img):
+    preferredWidth = 320
+    prefferedHeight = 240
+    currentWidth = len(img[0])
+    currentHeight = len(img)
+    print("Current Width: ", currentWidth)
+    print("Current Height: ", currentHeight)
+    tempWidth = tempHeight = 0
+    if currentWidth/currentHeight < preferredWidth/prefferedHeight:
+        tempWidth = preferredWidth
+        tempHeight = int(preferredWidth/currentWidth*currentHeight)
+    else:
+        tempHeight = prefferedHeight
+        tempWidth = int(prefferedHeight/currentHeight*currentWidth)
+    print(tempWidth, tempHeight)
+    img = cv2.resize(img, (tempWidth, tempHeight))
+    return img
+def crop(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Cropping an image
-    cropped_image = gray[80:200, 0:240]
-    width = 320
-    height = 240
-    points = (width, height)
-    resized = cv2.resize(cropped_image, points, interpolation= cv2.INTER_LINEAR)
+    prefWidth = 320
+    prefHeight = 240
+    currentWidth = len(img[0])
+    currentHeight = len(img)
+    #crop it equally from all sides
+    widthCrop = int((currentWidth - prefWidth)/2)
+    heightCrop = int((currentHeight - prefHeight)/2)
+    cropped_image = gray[heightCrop:currentHeight-heightCrop, widthCrop:currentWidth-widthCrop]
+    return cropped_image
 
-    return resized
+def preprocessing(img): 
+    img = crop(scale(img))
+    return img
 
 
 
-coconut = cv2.imread('Coconuts\coconut.png')
-coconut1 = cv2.imread('Coconuts\coconut1.png')
-coconut2 = cv2.imread('Coconuts\coconut2.png')
+coconut = cv2.imread('Coconuts\coconut2.png')
+#coconut1 = cv2.imread('Coconuts\coconut1.png')
+#coconut2 = cv2.imread('Coconuts\coconut2.png')
 
 #cv2.imshow('asd', coconut1)
 #cv2.waitKey()
 coconutRe = preprocessing(coconut)
-coconutRe1 = preprocessing(coconut1)
-coconutRe2 = preprocessing(coconut2)
+#coconutRe1 = preprocessing(coconut1)
+#coconutRe2 = preprocessing(coconut2)
 
 cv2.imshow('original image', coconut)
 cv2.imshow('resized image', coconutRe)
-cv2.imshow('resized1 image', coconutRe1)
-cv2.imshow('resized2 image', coconutRe2)
+#cv2.imshow('resized1 image', coconutRe1)
+#cv2.imshow('resized2 image', coconutRe2)
 cv2.waitKey()
