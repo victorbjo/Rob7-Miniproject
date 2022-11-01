60e7a888ef46b5cfb8b5a351f1e2b72145890dac
JimmiJensen <jijens19@student.aau.dk>
2022-11-01 10:05:25 +0100

    update to sepfilter

--- main.py
+++ main.py
@@ -5,30 +5,29 @@ import SepFilter
 import Preprocessing as pp
 def main(path): #Pretty much pseudo code
     picture = cv2.imread(path)
-    cv2.imshow('s', picture)
-    cv2.waitKey()
     picture = pp.preprocessing(picture)
     width = len(picture[0])
     height = len(picture)
 
     #Run function x for every 20 pixel
-    for y in range(0, height, 20):
-        for x in range(0, width, 20):
+    for y in range(0, height, 40):
+        for x in range(0, width, 40):
             sepFilterResults = SepFilter.sepFil(picture, x, y)
             for idx, result in enumerate(sepFilterResults):
                 if result >= 0.1:
                     r = idx*10+20
-                    HoughCircles(picture, r)
+                    #HoughCircles(picture, r)
                     print(result)
                     print(idx)
                     print(y)
                     print(x)
                     print(result)
-        cv2.imshow(picture)
-        cv2.waitKey()
-                                    
+        print(y)
+    cv2.imshow('a', picture)
+    cv2.waitKey()
+
 if __name__ == "__main__":
-    main('\Coconuts\coconut1.png')
+    main('Coconuts\test.png')
 
 '''
 TODO: We need to fix preprocessing, and make it easilier callable
