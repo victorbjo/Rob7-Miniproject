b31de90d77932b805382099727396663b1b4d90a
Dadgostar <mahed98@gmail.com>
2022-11-01 10:45:26 +0100

    updated circles

--- HoughCircle.py
+++ HoughCircle.py
@@ -3,97 +3,60 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import Preprocessing
-from circle import circleDrawing
+from circle import circleDrawing, distance_list
 import SepFilter as sp
 import pandas as pd
 
-width = 320
-height = 240
-R = 50
-rr = 23
+# width = 320
+# height = 240
+
+threshold_circle_check =  10
+
 img = cv2.imread('Coconuts\coconut.png')
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+def HoughCircles(picture, window_i, window_j, rr):
+    isCircle = 0
+    height, width= picture.shape
+# img1 = Preprocessing.preprocessing(img) # already preprocessed
 
-# dim = (width, height)
-# new_img = cv2.resize(img, dim)
-#cv2.imshow('graycsale image',img)
-#plt.imshow(new_img, cmap = 'gray')
-
-img1 = Preprocessing.preprocessing(img)
-
-plt.imshow(img1, cmap = 'gray')
-
+# plt.imshow(img1, cmap = 'gray')
 # img2 = sp.sepFil(img1,)
 
+    img_blank = np.zeros( picture.shape) 
+    # print(img_blank.shape)
+    # a_test = circleDrawing(290, 100, 35, width, height)
+    # plt.imshow(a_test, cmap='gray')
 
-# bordersize = R
-# img_bordered = cv2.copyMakeBorder(
-#     img1,
-#     top=bordersize,
-#     bottom=bordersize,
-#     left=bordersize,
-#     right=bordersize,
-#     borderType=cv2.BORDER_CONSTANT,
-#     value=[0]
-# )
+    # for i in range(0, width):
+    #     for j in range(0, height):
+    #         if picture[j, i]>0:
+    #             img_blank = img_blank + circleDrawing(  i, j, rr, width, height)     
 
 
+    # window_i,j are inputs (top left of the window)
+    for i in range(max(window_i - rr, 0), min(window_i + rr, width)):
+        for j in range(max(window_j - rr, 0), min(window_j + rr, height)):
+            if picture[j, i]>0:
+                img_blank = img_blank + circleDrawing(i, j, rr, width, height)
 
-# fig, ax = plt.subplots(1,2)
-# ax[0].imshow(img1, cmap = 'gray')
-# ax[0].set_title("original")
-# ax[1].imshow(img_bordered, cmap = 'gray')
-# ax[1].set_title("after padding")
-
-
-# cv2.imshow('picture prepocessed',border)
-# cv2.waitKey()
-
-
-#img_blank = np.zeros( img_bordered.shape) 
-#img_blank =  cv2.cvtColor(img_blank, cv2.COLOR_GRAY2BGR)
-#[2*R+height, 2*R+width]
-#plt.imshow( img_blank, cmap = 'gray')
-img_blank = np.zeros( img1.shape) 
-img_blank.shape
+    img_blank_larg_ind = largest_indices(img_blank, int(rr/4)) ###################### maybe change 2 * rr
+    mean_x = int(img_blank_larg_ind[1].sum()/rr)
+    mean_y = int(img_blank_larg_ind[0].sum()/rr)
+    img_blank_larg_val = np.mean(img_blank[img_blank_larg_ind])
+    if np.power(np.power(np.abs(window_i - mean_x),2)+np.power(np.abs(window_j - mean_y),2),0.5) < threshold_circle_check :
+        isCircle = 1
+    return isCircle, img_blank_larg_val, mean_x, mean_y
+        
+        
 
 
-# plt.imshow(circleDrawing(  10, 10, 10, width, height), cmap='gray')
-#plt.imshow(img_blank, cmap='gray')
+def largest_indices(ary, n):
+    """Returns the n largest indices from a numpy array."""
+    flat = ary.flatten()
+    indices = np.argpartition(flat, -n)[-n:]
+    indices = indices[np.argsort(-flat[indices])]
+    return np.unravel_index(indices, ary.shape)
 
-a_test = circleDrawing(50,100,20,width,height)
-a_test = a_test + img_blank
-plt.imshow(a_test, cmap='gray')
-a_test.shape
+# int(largest_indices(img_blank, 10)[0].sum()/10)
 
-
-
-for i in range(0, height):
-    for j in range(0, width):
-        if a_test[i,j]>0:
-            img_blank = img_blank + circleDrawing(  i, j, rr, width, height)     
-
-
-# ********** with padding
-#  rr = 23
-# for i in range(R, R+height):
-#     for j in range(R, R+width):
-#         if a_test[i,j]>0:
-#             img_blank = img_blank + circleDrawing(  i, j, rr, width, height)            
-
-
-plt.imshow(img_blank,cmap='gray')
-# print(img_blank[50:100,50:100])
-
-
-plt.hist(img_blank.ravel(),256,[0,256]); plt.show()
-
-
-new_img = img_blank[img_blank>10]
-plt.imshow(new_img,cmap='gray')
-# cv2.imwrite("circled.png",new_img)
-
-
-
-# 
